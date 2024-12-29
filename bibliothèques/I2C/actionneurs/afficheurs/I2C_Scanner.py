e5c72dee45fdc71e27e7202a50da0ed09e78aa4c
TMartial-prof <martial.prof.sii@gmail.com>
2024-12-29 15:09:21 +0100

    Update I2C_Scanner.py

--- bibliothèques/I2C/actionneurs/afficheurs/I2C_Scanner.py
+++ bibliothèques/I2C/actionneurs/afficheurs/I2C_Scanner.py
@@ -1,16 +1,13 @@
-# I2C Scanner MicroPython
 from machine import Pin, SoftI2C
 
-# You can choose any other combination of I2C pins
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
 print('I2C SCANNER')
 devices = i2c.scan()
 
 if len(devices) == 0:
-  print("No i2c device !")
+  print("Aucun composant i2c trouvé !")
 else:
-  print('i2c devices found:', len(devices))
-
+  print(len(devices),' composant(s) i2C trouvé(s) :')
   for device in devices:
-    print("I2C hexadecimal address: ", hex(device))
+    print("Adresse I2C hexadécimale : ", hex(device))
